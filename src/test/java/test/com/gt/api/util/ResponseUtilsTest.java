55d7a836a79c3057b27b0abf445ebe827307f37f
zhangmz <zhangmz@qq.com>
2017-08-03 11:50:52 +0800

    修改status 为 code 修改ResponseEnums 枚举类 desc为msg

--- src/test/java/test/com/gt/api/util/ResponseUtilsTest.java
+++ src/test/java/test/com/gt/api/util/ResponseUtilsTest.java
@@ -31,10 +31,10 @@ public class ResponseUtilsTest {
 	LOGGER.info( "成功数据格式+返回泛型数据：{}", om.writeValueAsString( ResponseUtils.createBySuccess( "XXXUser" ) ) );
 	LOGGER.info( "成功数据格式+定制消息+返回泛型数据：{}", om.writeValueAsString( ResponseUtils.createBySuccess( "获取到数据了", "XXXUser" ) ) );
 	LOGGER.info( "成功数据格式+定制状态码+定制消息+返回泛型数据：{}",
-			om.writeValueAsString( ResponseUtils.createBySuccessCodeMessage( ResponseEnums.SUCCESS.getCode(), ResponseEnums.SUCCESS.getDesc(), "XXXUser" ) ) );
+			om.writeValueAsString( ResponseUtils.createBySuccessCodeMessage( ResponseEnums.SUCCESS.getCode(), ResponseEnums.SUCCESS.getMsg(), "XXXUser" ) ) );
 	LOGGER.info( "失败数据格式：{}", om.writeValueAsString( ResponseUtils.createByError() ) );
 	LOGGER.info( "失败数据格式+定制消息：{}", om.writeValueAsString( ResponseUtils.createByErrorMessage( "参数缺失" ) ) );
-	LOGGER.info( "失败数据格式+定制状态码+定制消息：{}", om.writeValueAsString( ResponseUtils.createByErrorCodeMessage( ResponseEnums.SYSTEM_ERROR.getCode(), ResponseEnums.SYSTEM_ERROR.getDesc() ) ) );
+	LOGGER.info( "失败数据格式+定制状态码+定制消息：{}", om.writeValueAsString( ResponseUtils.createByErrorCodeMessage( ResponseEnums.SYSTEM_ERROR.getCode(), ResponseEnums.SYSTEM_ERROR.getMsg() ) ) );
     }
 
 }
